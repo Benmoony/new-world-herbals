6fa051028c4a6db5a3e1b9839208728496550a05
Benmoony <benmiller92@gmail.com>
2019-03-10 15:07:40 -0700

    Updates

---
+++ precache-manifest.84830013ea6d013e372b607779d0ae1a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "c6f069cda4367eb8fa7608cd730714e1",
+    "url": "/new-world-herbals/static/media/sage.c6f069cd.jpg"
+  },
+  {
+    "revision": "2902f9ad1a079a41cc94",
+    "url": "/new-world-herbals/static/css/main.a9e341bb.chunk.css"
+  },
+  {
+    "revision": "40ff67c788bf9fb793fd",
+    "url": "/new-world-herbals/static/js/runtime~main.dc243f7a.js"
+  },
+  {
+    "revision": "96fcbe893b6a5ac8e495",
+    "url": "/new-world-herbals/static/js/2.c16faeaf.chunk.js"
+  },
+  {
+    "revision": "6a55ae8377362a1cbcc3b57a4d045700",
+    "url": "/new-world-herbals/static/media/ginseng.6a55ae83.jpg"
+  },
+  {
+    "revision": "9bd04c95a2c6dd9c21ec540804266f73",
+    "url": "/new-world-herbals/static/media/FinalLogo.9bd04c95.png"
+  },
+  {
+    "revision": "2902f9ad1a079a41cc94",
+    "url": "/new-world-herbals/static/js/main.09a1251c.chunk.js"
+  },
+  {
+    "revision": "41db9eb692f9934e8700ed73fa3a2fd9",
+    "url": "/new-world-herbals/static/media/lavender.41db9eb6.jpg"
+  },
+  {
+    "revision": "ec04b3bdca684502388737158a1060c2",
+    "url": "/new-world-herbals/static/media/ginko.ec04b3bd.jpg"
+  },
+  {
+    "revision": "3630f000453f849c96848eb7f944e685",
+    "url": "/new-world-herbals/static/media/honey.3630f000.jpg"
+  },
+  {
+    "revision": "9b013628ba44a73185f9a0f947cb4c86",
+    "url": "/new-world-herbals/static/media/herbs.9b013628.jpg"
+  },
+  {
+    "revision": "a98044fc20abc140d25265ffc0423ab6",
+    "url": "/new-world-herbals/index.html"
+  }
+];
